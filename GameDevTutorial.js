914d3f32857b0f73efacced38a4cf1645aeb3306
Akshat-6046 <109698899+Akshat-6046@users.noreply.github.com>
2023-01-15 13:47:33 +0530

    commit03

--- GameDevTutorial.js
+++ GameDevTutorial.js
@@ -9,7 +9,7 @@ function setup() {
   paddle_y = height - 25;
   paddle_height = 15;
   vis=1;
-   ball_diameter = 20;
+  ball_diameter = 20;
   ball_dx =1;
   ball_dy = -2;
   paddle_dx = 3;
@@ -17,7 +17,7 @@ function setup() {
   ball_y = (height / 2) - (ball_diameter / 2);
   block_x=100;
   block_y=50;
-  block_width=paddle_width/2;
+  block_width=paddle_width*2;
   block_height=paddle_height;
 }
  
@@ -70,22 +70,22 @@ function draw () {
     ball_dx = 0;
      console.log("GAME OVER");
   }
-  let block=rect(block_x, block_y, block_width, block_height);
   
   
   if(vis==1){
-  if((ball_x<block_x+block_width) && 
+    let block=rect(block_x, block_y, block_width, block_height);
+  }
+  if(vis&&(ball_x<block_x+block_width) && 
     (ball_x>block_x) && 
     (ball_y<block_y+(block_height / 2))  && 
     (ball_y>block_y)){
     
      ball_dy=-ball_dy;
-     block_height=0;
-     block_height=0;
+     
     console.log("YOU WIN");
   
     vis=0;
- }
+ 
   }
   
  
